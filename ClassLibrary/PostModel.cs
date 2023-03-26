b308d90bc84e10fffc3f85608e34de082eae6a87
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-03-26 15:13:12 -0700

    The cards are now shown by a list

---
+++ ClassLibrary/PostModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class PostModel
+    {
+        public string Id { get; set; }
+        public string ImgURL { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public PostModel()
+        {
+        }
+
+        public PostModel(string id, string imgURL, string title, string description)
+        {
+            Id = id;
+            ImgURL = imgURL;
+            Title = title;
+            Description = description;
+        }
+    }
+}
--- Instagramp 2/Controllers/HomeController.cs
+++ Instagramp 2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ClassLibrary;
 
 namespace Instagramp_2.Controllers
 {
@@ -6,7 +7,11 @@ namespace Instagramp_2.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<PostModel> posts = new List<PostModel>();
+            posts.Add(new PostModel("sadjaw7as8da6sd", "https://hips.hearstapps.com/hmg-prod/images/alpe-di-siusi-sunrise-with-sassolungo-or-langkofel-royalty-free-image-1623254127.jpg", "Mountain Vilage", "This weekend we went up to the Antiolan mountain range near Antelago Columbia"));
+            posts.Add(new PostModel("sadjaw7as8da7sd", "https://hips.hearstapps.com/hmg-prod/images/ama-dablam-mountain-peak-view-from-chola-pass-royalty-free-image-1623254695.jpg", "Card title", "Some quick example text to build on the card title and make up the bulk of the card's"));
+            posts.Add(new PostModel("sadjaw7as8da8sd", "https://www.travelmanagers.com.au/wp-content/uploads/2012/08/AdobeStock_254529936_Railroad-to-Denali-National-Park-Alaska_750x500.jpg", "Card title", "Some quick example text to build on the card title and make up the bulk of the card's"));
+            return View(posts);
         }
     }
 }
