ac34467addf60731025b29584049608195258688
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-04-10 14:52:44 -0700

    Deleated some SQL connection we had before

--- Instagramp 2/Service/SecurityDAO.cs
+++ Instagramp 2/Service/SecurityDAO.cs
@@ -7,7 +7,6 @@ namespace RegisterAndLoginApp.Service
 {
     public class SecurityDAO
     {
-        //string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         private readonly IMongoCollection<UserModel> _users;
 
@@ -52,37 +51,5 @@ namespace RegisterAndLoginApp.Service
             var filter = Builders<UserModel>.Filter.Eq(u => u.Id, id);
             _users.DeleteOne(filter);
         }
-
-        //public bool FindUserByNameAndPassword(UserModel user)
-        //{
-        //    bool success = false;
-
-        //    string sqlStatement = "select * from dbo.users where Username = @username and Password = @password";
-
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        SqlCommand command = new SqlCommand(sqlStatement, connection);
-
-        //        command.Parameters.Add("@USERNAME", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
-        //        command.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
-
-        //        try
-        //        {
-        //            connection.Open();
-        //            SqlDataReader reader = command.ExecuteReader();
-
-        //            if (reader.HasRows)
-        //            {
-        //                success = true;
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-        //    }
-        //    return success;
-        //}
-
     }
 }
