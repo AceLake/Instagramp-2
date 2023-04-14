fa900ea8489243e11add71ce46c5db624bebdaa1
JC949 <jacobcampbell180@yahoo.com>
2023-04-14 16:10:26 -0700

    Fixed merge conflicts

--- Instagramp 2/wwwroot/js/site.js
+++ Instagramp 2/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    console.log("Page Ready");
 
 // Write your JavaScript code.
+    //$(".create-post").click(function (event) {
+    //    event.preventDefault();
+    //    console.log("created button was clicked")
+    //    doPostCreate();
+    //});
+
+    //function doPostCreate() {
+    //    $.ajax({
+    //        datatype: "json",
+    //        url: 'Profile/AddPost',
+    //        data: $("form").serialize(),
+    //        success: function (data) {
+    //            console.log(data);
+    //        }
+    //    });
+    //};
+});
