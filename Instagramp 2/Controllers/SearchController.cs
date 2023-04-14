20aa2530e85ddec36c230663476247a89189b1f3
JC949 <jacobcampbell180@yahoo.com>
2023-04-14 16:05:38 -0700

    Edited search page css

--- Instagramp 2/Controllers/SearchController.cs
+++ Instagramp 2/Controllers/SearchController.cs
@@ -9,12 +9,12 @@ namespace Instagramp_2.Controllers
         {
             List<SearchTermModel> searchTerms = new List<SearchTermModel>();
 
-            searchTerms.Add(new SearchTermModel("Family", "See posts about family!", "https://upload.wikimedia.org/wikipedia/commons/d/df/Family_Portrait.jpg"));
-            searchTerms.Add(new SearchTermModel("Friends", "See posts about friends!", "friendspic.jpg"));
-            searchTerms.Add(new SearchTermModel("Nature", "See posts about nature!", "naturepic.jpg"));
-            searchTerms.Add(new SearchTermModel("Sports", "See posts about sports!", "sportspic.jpg"));
-            searchTerms.Add(new SearchTermModel("Food", "See posts about food!", "foodpic.jpg"));
-            searchTerms.Add(new SearchTermModel("Other", "See posts about other!", "otherpic.jpg"));
+            searchTerms.Add(new SearchTermModel("Family", "See posts about family!", "img/familypic.jpg"));
+            searchTerms.Add(new SearchTermModel("Friends", "See posts about friends!", "img/friendspic.jpg"));
+            searchTerms.Add(new SearchTermModel("Nature", "See posts about nature!", "img/naturepic.jpg"));
+            searchTerms.Add(new SearchTermModel("Sports", "See posts about sports!", "img/sportspic.jpg"));
+            searchTerms.Add(new SearchTermModel("Food", "See posts about food!", "img/foodpic.jpg"));
+            searchTerms.Add(new SearchTermModel("Other", "See posts about all other topics!", "img/otherpic.jpg"));
 
             return View(searchTerms);
         }
