5bf35ff9085f3cff4481a6e6f01246be49ddcb41
JC949 <jacobcampbell180@yahoo.com>
2023-04-17 10:13:47 -0700

    Search functionality

--- Instagramp 2/Controllers/SearchController.cs
+++ Instagramp 2/Controllers/SearchController.cs
@@ -9,14 +9,43 @@ namespace Instagramp_2.Controllers
         {
             List<SearchTermModel> searchTerms = new List<SearchTermModel>();
 
-            searchTerms.Add(new SearchTermModel("Family", "See posts about family!", "img/familypic.jpg"));
-            searchTerms.Add(new SearchTermModel("Friends", "See posts about friends!", "img/friendspic.jpg"));
-            searchTerms.Add(new SearchTermModel("Nature", "See posts about nature!", "img/naturepic.jpg"));
-            searchTerms.Add(new SearchTermModel("Sports", "See posts about sports!", "img/sportspic.jpg"));
-            searchTerms.Add(new SearchTermModel("Food", "See posts about food!", "img/foodpic.jpg"));
-            searchTerms.Add(new SearchTermModel("Other", "See posts about all other topics!", "img/otherpic.jpg"));
+            searchTerms.Add(new SearchTermModel("Family", "See posts about family!", "img/familypic.jpg", "Family", "Search"));
+            searchTerms.Add(new SearchTermModel("Friends", "See posts about friends!", "img/dawgsoutpic.jpg", "Friends", "Search"));
+            searchTerms.Add(new SearchTermModel("Nature", "See posts about nature!", "img/naturepic.jpg", "Nature", "Search"));
+            searchTerms.Add(new SearchTermModel("Sports", "See posts about sports!", "img/sportspic.jpg", "Sports", "Search"));
+            searchTerms.Add(new SearchTermModel("Food", "See posts about food!", "img/foodpic.jpg", "Food", "Search"));
+            searchTerms.Add(new SearchTermModel("Other", "See posts about all other topics!", "img/otherpic.jpg", "Other", "Search"));
 
             return View(searchTerms);
+        }
+
+        public IActionResult Family()
+        {
+            return View("Family");
+        }
+        public IActionResult Friends()
+        {
+            return View("Friends");
+        }
+
+        public IActionResult Nature()
+        {
+            return View("Nature");
+        }
+
+        public IActionResult Sports()
+        {
+            return View("Sports");
+        }
+
+        public IActionResult Food()
+        {
+            return View("Food");
+        }
+
+        public IActionResult Other()
+        {
+            return View("Other");
         }
     }
 }
--- Instagramp 2/Models/SearchTermModel.cs
+++ Instagramp 2/Models/SearchTermModel.cs
@@ -6,11 +6,18 @@
         public string description { get; set; }
         public string imgURL { get; set; }
 
-        public SearchTermModel(string name, string description, string imgURL)
+        public string actionName { get; set; }
+
+        public string controllerName { get; set; }
+
+
+        public SearchTermModel(string name, string description, string imgURL, string actionName, string controllerName)
         {
             this.name = name;
             this.description = description;
             this.imgURL = imgURL;
+            this.actionName = actionName;
+            this.controllerName = controllerName;
         }
         public SearchTermModel()
         {
